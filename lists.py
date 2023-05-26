0e8cd2171b8fe87f3cfcd4515e4e6e8ec55545a1
Nicole Williams <nicole.boesenberg@gmail.com>
2023-05-25 20:26:49 -0700

    added smallest_int function

--- lists.py
+++ lists.py
@@ -83,8 +83,15 @@ def smallest_int(numbers):
         >>> smallest_int([]) is None
         True
     """
+    if numbers == []:
+        return None
 
-    return 100
+    current_min = numbers[0]
+    for num in numbers:
+        if num < current_min:
+            current_min = num
+
+    return current_min 
 
 
 def largest_int(numbers):
